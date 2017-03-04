6c3735e39de92ca5a492a5c6b34c1d51abee76b7
Jan Freyberg <jan.freyberg@gmail.com>
2017-03-04 15:15:22 +0000

    Include color map coercion
    doesn't yet work for all, but also displays all colormaps if none is provided, with viridis still default

--- niwidgets.py
+++ niwidgets.py
@@ -11,14 +11,14 @@ class NiWidget:
         self.filename = filename
         
     
-    def plot_slices(self,data,x, y, z, Colormap='viridis',figsize=(15,5)):
+    def plot_slices(self,data,x, y, z, colormap='viridis',figsize=(15,5)):
         """
         plots x,y,z slices
         """
         fig,axes=plt.subplots(1,3, figsize = figsize)
-        axes[0].imshow(np.rot90(data[:,y,:]), cmap = Colormap)
-        axes[1].imshow(np.rot90(data[x,:,:]), cmap = Colormap)
-        axes[2].imshow(np.rot90(data[:,:,z]), cmap = Colormap)
+        axes[0].imshow(np.rot90(data[:,y,:]), cmap = colormap)
+        axes[1].imshow(np.rot90(data[x,:,:]), cmap = colormap)
+        axes[2].imshow(np.rot90(data[:,:,z]), cmap = colormap)
         plt.show()
         
     def default_plotter(self,**kwargs):
@@ -26,38 +26,56 @@ class NiWidget:
         basic plot function to be used if no custom function is specified
         """
         
+        # load data in advance
         self.data = nib.load(self.filename).dataobj
         
-        if len(kwargs) == 0:
-            kwargs = {'x': (0,self.data.shape[0]-1),
-                         'y': (0,self.data.shape[1]-1),
-                         'z': (0,self.data.shape[2]-1),
-                         'Colormap': ['viridis','jet','gray'],
-                         'figsize': fixed((15,5))
-                     }
+        # set default x y z values
+        for dim, label in enumerate(['x', 'y', 'z']):
+            if label not in kwargs.keys():
+                kwargs[label] = (0,self.data.shape[dim]-1)
+        
+        # set default colormap
+        if 'colormap' not in kwargs.keys():
+            kwargs['colormap'] = ['viridis'] + sorted(m for m in plt.cm.datad if not m.endswith("_r"))
+        elif len([kwargs['colormap']]) == 1:
+            # fix cmap if only one given
+            kwargs['colormap'] = fixed(kwargs['colormap'])
+        
+        # set default figure size
+        if 'figsize' not in kwargs.keys():
+            kwargs['figsize'] = fixed((15,5))
             
-        interact(self.plot_slices,
-                data = fixed(self.data),**kwargs)
+        interact(self.plot_slices, data = fixed(self.data), **kwargs)
 
         
-    def _custom_plot_wrapper(self,data,**kwargs):
+    def _custom_plot_wrapper(self, data, **kwargs):
         """
         plot wrapper for custom function
         """
+        if 'colormap' in kwargs.keys():
+            self.colormap = kwargs['colormap']
+            kwargs.pop('colormap', None)
         self.plotting_func(data,**kwargs)
+        plt.set_cmap(self.colormap)
         plt.show()
 
     def custom_plotter(self,plotting_func,**kwargs):
         """
         collects data and starts interactive widget for custom plot
         """
-        
+        if 'colormap' not in kwargs.keys():
+            kwargs['colormap'] = ['viridis'] + sorted(m for m in plt.cm.datad if not m.endswith("_r"))
+        elif len([kwargs['colormap']]) == 1:
+            # fix cmap if only one given
+            kwargs['colormap'] = fixed(kwargs['colormap'])
+
         #gets name first argument, assuming this takes the filename 
-#         argname = inspect.getargspec(plotting_func).args[0]
-#         kwargs[argname] = fixed(self.filename
-#         interact(self._custom_plot_wrapper,plotting_func=fixed(plotting_func),**kwargs)
+        #argname = inspect.getargspec(plotting_func).args[0]
+        #kwargs[argname] = fixed(self.filename
+        #interact(self._custom_plot_wrapper,plotting_func=fixed(plotting_func),**kwargs)
         self.plotting_func = plotting_func
         self.data = nib.load(self.filename)
+        
         interact(self._custom_plot_wrapper, data=fixed(self.data), **kwargs)
         
         
