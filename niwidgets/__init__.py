cd1693fc2726d4233070332557c84f02c5275b2c
BS <bs538@ast.cam.ac.uk>
2017-03-05 13:15:04 +0000

    further clean-up, added examples

--- niwidgets/__init__.py
+++ niwidgets/__init__.py
@@ -1,11 +1,5 @@
-import os
-
-# define the example data
-root_dir, _ = os.path.split(__file__)
-exampleatlas = os.path.join(root_dir, 'data', 'cc400_roi_atlas.nii')
-examplezmap = os.path.join(root_dir, 'data', 'cognitive control_pFgA_z_FDR_0.01.nii.gz')
-examplet1 = os.path.join(root_dir, 'data', 'T1.nii.gz')
-
+#import example data
+from .exampledata import exampleatlas,examplezmap,examplet1
 #import main class
 from .niwidget import NiWidget
 
