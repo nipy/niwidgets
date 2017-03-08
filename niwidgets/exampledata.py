2ca82a1feaa5d3f888d7c477337732525ff08796
Jan Freyberg <jan.freyberg@gmail.com>
2017-03-08 18:30:35 +0000

    Change z-map to unthresholded

--- niwidgets/exampledata.py
+++ niwidgets/exampledata.py
@@ -3,5 +3,5 @@ import os
 # define the example data
 root_dir, _ = os.path.split(__file__)
 exampleatlas = os.path.join(root_dir, 'data', 'cc400_roi_atlas.nii')
-examplezmap = os.path.join(root_dir, 'data', 'cognitive control_pFgA_z_FDR_0.01.nii.gz')
+examplezmap = os.path.join(root_dir, 'data', 'cognitive control_pFgA_z.nii.gz')
 examplet1 = os.path.join(root_dir, 'data', 'T1.nii.gz')
