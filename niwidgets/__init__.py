40933e1af01fa9436f009815ce67136539f48ae9
Jan Freyberg <jan.freyberg@gmail.com>
2017-08-11 17:11:18 +0100

    Generated gh-pages for commit 158cd03 Author: Jan Freyberg <jan.freyberg@gmail.com>
        Fix doc issues and import error

--- niwidgets/__init__.py
+++ niwidgets/__init__.py
@@ -1,4 +1,4 @@
 # import example data
-from .exampledata import exampleatlas, examplezmap, examplet1, examplesurf
+from .exampledata import exampleatlas, examplezmap, examplet1
 # import main class
 from .niwidget import NiftiWidget
