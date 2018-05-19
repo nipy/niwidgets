035b9df2048671c225dbc4cbacd85f3bc084ca01
Jan Freyberg <jan.freyberg@gmail.com>
2018-05-19 08:35:14 +0100

    Added pytest as a testing requirement

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 """Installer."""
-from setuptools import setup
+import os.path
 # To use a consistent encoding
 from codecs import open
-import os.path
+
+from setuptools import setup
 
 here = os.path.dirname(os.path.abspath(__file__))
 ver_file = os.path.join(here, 'niwidgets', 'version.py')
@@ -40,4 +41,7 @@ setup(
              'data/examples_surfaces/lh.*',
              'data/examples_surfaces/*.ctab']
     },
+    tests_require=[
+        'pytest'
+    ]
 )
