4c742b4275141770598eb7ac7ca91c5076bb6d59
Jan Freyberg <jan.freyberg@gmail.com>
2018-05-19 08:44:41 +0100

    Not testing sphinx docs for flake8 compliance

--- niwidgets/__init__.py
+++ niwidgets/__init__.py
@@ -4,7 +4,7 @@ Widgets to visualise neuroimaging data.
 For volume images, try import NiftiWidget.
 For surface images, try SurfaceWidget.
 """
-from .version import __version__
+from .version import __version__  # noqa: F401
 from .exampledata import exampleatlas, examplezmap, examplet1  # noqa: F401
 from .niwidget_volume import NiftiWidget  # noqa: F401
 from .niwidget_surface import SurfaceWidget  # noqa: F401
